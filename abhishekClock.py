c2f6acf0bd3a56c9e43b58fd254b55b5d4175cae
Dhruv Kaushik <93424205+dhruvk1998@users.noreply.github.com>
2023-03-18 18:59:05 +0530

    Add files via upload

--- abhishekClock.py
+++ abhishekClock.py
@@ -4,12 +4,14 @@ import time
 
 def wait_for_working_hours(start_time, end_time):
     while True:
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        now = datetime.datetime.now(tz=None).time()
         start = datetime.datetime.strptime(start_time, "%H:%M").time()
+        now = datetime.datetime.now(tz=None).time()
         end = datetime.datetime.strptime(end_time, "%H:%M").time()
+        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
         wait_time = datetime.timedelta(hours=6)
-        start_plus_6_hours = datetime.datetime.strptime(start_time, "%H:%M") + datetime.timedelta(hours=6)
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        start_plus_6_hours = datetime.datetime.combine(tomorrow, now) + datetime.timedelta(hours=6)
+        time_to_wait = (datetime.datetime.combine(tomorrow, now) - datetime.datetime.now())
         if now < start:
             # working hours have not started yet, wait until start_time
             time_to_wait = (datetime.datetime.combine(datetime.date.today(), start)- datetime.datetime.combine(datetime.date.today(), now)).total_seconds()
@@ -17,29 +19,25 @@ def wait_for_working_hours(start_time, end_time):
             time.sleep(time_to_wait)
         elif now >= end:
             # working hours are over, wait until tomorrow
-            tomorrow = datetime.date.today() + datetime.timedelta(days=1)
             # start_tomorrow = datetime.datetime.combine(tomorrow, start).time()
             time_to_wait = (datetime.datetime.combine(tomorrow, now) - datetime.datetime.now()).total_seconds()
-            print(
-                f"{now}.  Working hours are over. Waiting {time_to_wait} seconds until tomorrow... "
-            )
+            print(f"{now}.  Working hours are over. Waiting {time_to_wait} seconds until tomorrow... ")
             time.sleep(time_to_wait)
         else:
             # working hours are ongoing
-            start_plus_6_hours = datetime.datetime.combine(datetime.date.today(), now) + datetime.timedelta(hours=6)
-            print(f"Initiated on: {now},\n\nEstimated Time: {start_plus_6_hours}\n")
+            estimated_time = datetime.datetime.combine(datetime.date.today(), start) + datetime.timedelta(days=1) 
+            print(f"Initiated on: {now},\n\nEstimated Time: {estimated_time}\n")
             if start_plus_6_hours < datetime.datetime.now():
                 # it's already past the time to update leads, wait until tomorrow
-                tomorrow = datetime.date.today() + datetime.timedelta(days=1)
                 # start_tomorrow = datetime.datetime.combine(tomorrow, start).time()
                 time_to_wait = (datetime.datetime.combine(tomorrow, now)- datetime.datetime.now(tz=None)).total_seconds()
-                print(f"{now}. Working hours are ongoing, but it's too late to update leads. Waiting {wait_time} Hours until tomorrow... ")
+                print(f"{now}. Working hours are ongoing, but it's too late to update leads. Waiting {start_plus_6_hours} Hours until tomorrow... ")
                 time.sleep(time_to_wait)
             else:
                 # calculate the time to wait before updating leads
                 time_to_wait = (start_plus_6_hours - datetime.datetime.now(tz=None)).total_seconds()
                 print(f"{timestamp}. Working hours are ongoing. Waiting {wait_time} Hours before updating leads... ")
-                time.sleep(time_to_wait)
+                time.sleep(6*60*60)
                 # update the leads
                 print(f"{now}. Updating leads... ")
                 # wait for another 6 hours before updating leads again
